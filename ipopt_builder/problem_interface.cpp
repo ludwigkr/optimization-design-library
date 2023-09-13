f69a37c60bcbfb14ca53b23b4fc93b1322c1a361
ludwigkr <ludwigkr@mailbox.org>
2023-09-13 17:56:39 +0200

    Ipopt: Autogenerate itnerface

---
+++ ipopt_builder/problem_interface.cpp
@@ -0,0 +1,28 @@
+#include "problem_formulation_interface.h"
+#include <iostream>
+
+Problem_Interface::Problem_Interface() {
+    mynlp = new ProblemFormulation(&scenario, &prob_param, &xopt);
+    app = IpoptApplicationFactory();
+    status;
+}
+
+int Problem_Interface::solve() {
+    status = app->Initialize();
+    if (status != Solve_Succeeded) {
+        std::cout << std::endl << std::endl << "*** Error during initialization!" << std::endl;
+        return (int) status;
+    }
+
+    status = app->OptimizeTNLP(mynlp);
+
+    if (status == Solve_Succeeded) {
+        Index iter_count = app->Statistics()->IterationCount();
+        std::cout << std::endl << std::endl << "*** The problem solved in " << iter_count << " iterations!" << std::endl;
+
+        Number final_obj = app->Statistics()->FinalObjective();
+        std::cout << std::endl << std::endl << "*** The final value of the objective function is " << final_obj << '.' << std::endl;
+    }
+
+    return (int) status;
+}
