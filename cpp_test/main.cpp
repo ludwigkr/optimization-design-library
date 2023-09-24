1417e00d26ee285c01af681428dee53fc4e57270
ludwigkr <ludwigkr@mailbox.org>
2023-09-24 08:37:30 +0200

    Clean up

--- cpp_test/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <CppUTest/CommandLineTestRunner.h>
-
-int main(int argc, char **argv) {
-    MemoryLeakWarningPlugin::turnOffNewDeleteOverloads();
-    return  CommandLineTestRunner::RunAllTests(argc,argv);
-}
