67f9beba7ca91aefca30250e6a98c5927d439097
ludwigkr <ludwigkr@mailbox.org>
2023-09-24 21:51:59 +0200

    Use Regexp for replacing variables

--- optimizationsolver.py
+++ optimizationsolver.py
@@ -53,6 +53,7 @@ def definition_export(op: OptimizationProblem, custome_defines="", trajectory_pa
         f.write(header_content)
 
 def build_solver(op: OptimizationProblem, opts):
+    forbidden_variables = ['lam', 'x', 'xopt', 'param']
 
     assert(op.objective is not None)
     assert(not op.fn_initial_guess.function.is_null())
@@ -61,16 +62,13 @@ def build_solver(op: OptimizationProblem, opts):
     assert(not op.fn_lbg.function.is_null())
     assert(not op.fn_ubg.function.is_null())
     for scenario in op.scenario_parameters.names:
-        for xopt in op.optvars.names:
-            assert xopt not in scenario, f"{xopt} in {scenario}, please change either of the variable names."
+        assert scenario not in forbidden_variables, f"{scenario} in {forbidden_variables =}, please change your variable name."
 
     for prob_param in op.problem_parameters.names:
-        for xopt in op.optvars.names:
-            assert xopt not in prob_param, f"{xopt} in {prob_param}, please change either of the variable names."
+        assert prob_param not in forbidden_variables, f"{prob_param} in {forbidden_variables =}, please change your variable name."
 
-    for prob_param in op.problem_parameters.names:
-        for scenario in op.scenario_parameters.names:
-            assert prob_param not in scenario, f"{scenario} in {prob_param}, please change either of the variable names."
+    for xopt in op.optvars.names:
+        assert xopt not in forbidden_variables, f"{xopt} in {forbidden_variables =}, please change your variable name."
 
     op.fn_params.build('fn_problem_parameters', op.problem_parameters, op.scenario_parameters, op.problem_parameters.variables_flat())
     # assert(not op.fn_initial_guess.is_null())
