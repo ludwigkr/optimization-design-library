13dafeba065892e1ce5bf95a37b19b845464206a
ludwigkr <ludwigkr@mailbox.org>
2024-04-28 15:08:51 +0200

    Fix scalar expression parser

--- helpers/scalar_expression_parser.py
+++ helpers/scalar_expression_parser.py
@@ -1,11 +1,14 @@
+
 #!/usr/bin/env python3
 import casadi
 from dense_expression_parser import parse_dense_casadi_expression
 
 def remove_matrix_information_lhs(exp: str) -> str:
-    exp_split = exp.split("=")
+    lines = exp.split("\n")
+    exp_split = lines[-2].split("=")
     val_name = exp_split[0].split("[")[0].replace(" ", "")
-    exp = val_name + " =" + exp_split[1]
+    lines[-2] = val_name + " =" + exp_split[1]
+    exp = "\n".join(lines)
     return exp
 
 def parse_scalar_casadi_expression(name: str, exp: casadi.SX) -> str:
