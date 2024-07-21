5a2b48fd373ba1ada25ede2609fef930c5866c74
ludwigkr <ludwigkr@mailbox.org>
2024-07-21 22:45:24 +0200

    Update test_parse_om_function_to_casadi

--- unittest/test_parse_om_function_to_casadi.py
+++ unittest/test_parse_om_function_to_casadi.py
@@ -13,21 +13,30 @@ from parse_om_function_to_casadi import parse_om_function_to_casadi
 class TestParseOMFunctionToCasadi(unittest.TestCase):
     def setUp(self):
         self.x = Variables()
-        self.x.register('spring2_s_rel')
-        self.x.register('spring2_s_rel0')
+        self.x.register('value_b')
+        self.x.register('value_a')
         self.u = Variables()
         self.params = Variables()
-        self.params.register('spring2_c')
+        self.params.register('value_c')
         self.model_vars = ModelVariables(self.x, self.u, self.params)
 
     def test_parse_om_function_to_casadi(self):
-        func = parse_om_function_to_casadi(self.model_vars, "spring2_c * (spring2_s_rel - spring2_s_rel0);")
+        func = parse_om_function_to_casadi(self.model_vars, "value_c * (value_b - value_a);")
         self.assertTrue(type(func) == casadi.Function)
-        func = parse_om_function_to_casadi(self.model_vars, "spring2_c * (spring2_s_rel - spring2_s_rel0)")
+        func = parse_om_function_to_casadi(self.model_vars, "value_c * (value_b - value_a)")
+        self.assertTrue(type(func) == casadi.Function)
+
+    def test_parse_function_with_numbers_to_casadi(self):
+        func = parse_om_function_to_casadi(self.model_vars, "value_c * (2./value_b - value_a);")
+        x = self.x.unpacked([1,0.3])
+        u = []
+        p = self.params.unpacked([0.4])
+        ret = func(x,u,p)
+        self.assertTrue(ret == 0.68)
         self.assertTrue(type(func) == casadi.Function)
 
     def test_om_function_features(self):
-        func = parse_om_function_to_casadi(self.model_vars, "spring2_c * (spring2_s_rel - spring2_s_rel0)")
+        func = parse_om_function_to_casadi(self.model_vars, "value_c * (value_b - value_a)")
 
         with self.subTest("Vector handling"):
             # Each new state gets it's own row
